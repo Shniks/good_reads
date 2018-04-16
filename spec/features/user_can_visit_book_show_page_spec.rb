367ec2e196effc94f8f8b2b6fadeb4b9d5203714
Lihkin <shniks@gmail.com>
2018-04-16 07:10:12 -0600

    Created passing test for the title, user name, rating and body on the book show page story.

--- spec/features/user_can_visit_book_show_page_spec.rb
+++ spec/features/user_can_visit_book_show_page_spec.rb
@@ -2,22 +2,25 @@ require 'rails_helper'
 
 describe 'As a User' do
   describe 'When I visit a book\'s show page' do
+    before(:each) do
+      @book = Book.create!(title: 'Harry Potter')
+      @user_1 = @book.users.create!(name: 'Nikhil')
+      @user_2 = @book.users.create!(name: 'Sergio')
+      @review_1 = @book.reviews.create!(body: 'body1', rating: 1, user_id: @user_1.id)
+      @review_2 = @book.reviews.create!(body: 'body2', rating: 2, user_id: @user_2.id)
+    end
+
     scenario 'I can see the book title, list of review, username, body and rating for that book' do
-      book = Book.create!(title: 'Harry Potter')
-      user_1 = book.users.create!(name: 'Nikhil')
-      user_2 = book.users.create!(name: 'Sergio')
-      review_1 = book.reviews.create!(body: 'body1', rating: 1, user_id: user_1.id)
-      review_2 = book.reviews.create!(body: 'body2', rating: 2, user_id: user_2.id)
 
-      visit book_path(book)
+      visit book_path(@book)
 
-      expect(page).to have_content(user_1.name)
-      expect(page).to have_content(user_2.name)
-      expect(page).to have_content(book.title)
-      expect(page).to have_content(review_1.body)
-      expect(page).to have_content(review_1.rating)
-      expect(page).to have_content(review_2.body)
-      expect(page).to have_content(review_2.rating)
+      expect(page).to have_content(@user_1.name)
+      expect(page).to have_content(@user_2.name)
+      expect(page).to have_content(@book.title)
+      expect(page).to have_content(@review_1.body)
+      expect(page).to have_content(@review_1.rating)
+      expect(page).to have_content(@review_2.body)
+      expect(page).to have_content(@review_2.rating)
     end
   end
 end
