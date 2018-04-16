03e70c380723a27ed762ca496c0d35cd05a3871c
Lihkin <shniks@gmail.com>
2018-04-16 07:12:56 -0600

    Created passing test for average rating.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
+  def average_rating
+    reviews.average(:rating)
+  end
 end
--- spec/features/user_can_visit_book_show_page_spec.rb
+++ spec/features/user_can_visit_book_show_page_spec.rb
@@ -7,7 +7,7 @@ describe 'As a User' do
       @user_1 = @book.users.create!(name: 'Nikhil')
       @user_2 = @book.users.create!(name: 'Sergio')
       @review_1 = @book.reviews.create!(body: 'body1', rating: 1, user_id: @user_1.id)
-      @review_2 = @book.reviews.create!(body: 'body2', rating: 2, user_id: @user_2.id)
+      @review_2 = @book.reviews.create!(body: 'body2', rating: 3, user_id: @user_2.id)
     end
 
     scenario 'I can see the book title, list of review, username, body and rating for that book' do
@@ -21,6 +21,13 @@ describe 'As a User' do
       expect(page).to have_content(@review_1.rating)
       expect(page).to have_content(@review_2.body)
       expect(page).to have_content(@review_2.rating)
+    end
+
+    scenario 'I can see the average rating for that book' do
+
+      visit book_path(@book)
+
+      expect(page).to have_content(@book.average_rating)
     end
   end
 end
